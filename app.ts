954b4feff424f8e643465f62d0652fcc58a2ad95
Jason Simpson <jason.simpson@middleware.co.nz>
2024-01-31 14:17:58 +1300

    add typescript

---
+++ app.ts
@@ -0,0 +1,11 @@
+import * as http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, TypeScript and Node.js!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
